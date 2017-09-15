405496c33afc5cedb7a970fab40497c32c49dfe3
F343224963 <v-dofu@microsoft.com>
2017-09-15 14:03:03 +0800

    init index

--- js/site.js
+++ js/site.js
@@ -1,15 +1,6 @@
-$(function(){
-    $(window).scroll(function(event){
-        var scrollTop=document.body.scrollTop;
-        if(scrollTop>364){
-            $(".mynav").css("background-color","#fff");
-            $(".mynav a").css("color","#000");
-            $(".mynav").css("box-shadow","0px 1px 10px rgb(244,244,244)");
-        }else{
-            $(".mynav").css("background-color","#030303");
-            $(".mynav a").css("color","#fff");
-            $(".mynav").css("box-shadow","0px 0px 0px #000");
-        }        
-
-    });
-})
+$(function() {
+    $('.loading').delay(500).fadeOut("slow");
+    console.log('Congratulations!!!');
+    console.log('Hi,********* is Microsoft');
+    console.log('You can find me through the following tel Numbers c7fe30ec2d00235508896ed488b7f3ab');
+});
