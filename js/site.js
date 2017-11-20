83d0cb9a822d74a14cb614ea98841db0387d7832
Cream2015 <343224963@qq.com>
2017-11-20 10:57:23 +0800

    revise js

--- js/site.js
+++ js/site.js
@@ -3,9 +3,10 @@ $(function() {
     console.log('Congratulations!!!');
     console.log('You can find me through the following tel Numbers c7fe30ec2d00235508896ed488b7f3ab');
     var count = 0;
+var currentMonth = new Date().getUTCMonth() + 1;
     $("#avatar-img").click(function() {
         count++;
-        if (count == 32) {
+        if (count == currentMonth) {
             $(".h5Event").html('Microsoft G+E');
         }
     });
